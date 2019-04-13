282daf9d60eb1726724877a5b66f9b4ab3a84d87
small <37457861+zmall53@users.noreply.github.com>
2019-04-13 18:37:25 +0100

    Removed Generation Delay
    Removed the delay and animation before generating numbers

--- main.ts
+++ main.ts
@@ -1,24 +1,29 @@
 let output = 0
 let AntiNegIn = 0
-let numb2 = 0
-let numb1 = 0
 let numbOutput = 0
+let numb2 = 0
 let numbInput = ""
+let numb1 = 0
 let loadAnim = 0
 let length = 0
 let setAnim = 0
 let isGen = 0
 let onSet = 0
-function gen()  {
+input.onPinPressed(TouchPin.P0, function () {
+    if (isGen == 0) {
+        serial.writeLine("NumbGen 0.0.1 | System Stats")
+        serial.writeLine("Device Name: " + control.deviceName())
+        serial.writeLine("Device Serial: " + control.deviceSerialNumber())
+        serial.writeLine("Temp: " + input.temperature())
+    }
+})
+function gen2() {
     serial.writeLine("Generating... with a length of " + length)
     serial.writeLine("")
-    loadAnim = 1
-    basic.pause(4000)
-    loadAnim = 0
     for (let i = 0; i < length; i++) {
-        numb1 = (Math.random(20001) + 1) * (input.temperature() + 1)
+        numb1 = (Math.randomRange(0, 20001) + 1) * (input.temperature() + 1)
         numb2 = numb1 * (input.lightLevel() + 1)
-        AntiNegIn = numb2 * (Math.random(56) + 1)
+        AntiNegIn = numb2 * (Math.randomRange(0, 56) + 1)
         output = custom.antiNeg(AntiNegIn)
         serial.writeNumber(output)
     }
@@ -29,11 +34,11 @@ function gen()  {
     basic.pause(0)
     isGen = 0
 }
-input.onButtonPressed(Button.A, () => {
+input.onButtonPressed(Button.A, function () {
     onSet = 0
     isGen = 1
 })
-input.onButtonPressed(Button.B, () => {
+input.onButtonPressed(Button.B, function () {
     if (isGen == 0) {
         setAnim = 1
         serial.writeLine("Enter length as \" [NUMB]# \" NOTE: MUST BE A NUMBER ")
@@ -47,26 +52,18 @@ input.onButtonPressed(Button.B, () => {
         basic.showIcon(IconNames.Yes)
     }
 })
-input.onPinPressed(TouchPin.P0, () => {
-    if (isGen == 0) {
-        serial.writeLine("NumbGen 0.0.1 | System Stats")
-        serial.writeLine("Device Name: " + control.deviceName())
-        serial.writeLine("Device Serial: " + control.deviceSerialNumber())
-        serial.writeLine("Temp: " + input.temperature())
-    }
-})
 serial.writeLine(control.deviceName())
 serial.writeNumber(control.deviceSerialNumber())
 serial.writeLine("")
 serial.writeLine("NumbGen 0.0.1 | Press A to begin | Press B to set length")
 length = 5
 isGen = 0
-basic.forever(() => {
+basic.forever(function () {
     if (loadAnim == 1) {
-        led.plot(Math.random(5), Math.random(5))
+        led.plot(Math.randomRange(0, 5), Math.randomRange(0, 5))
     }
 })
-basic.forever(() => {
+basic.forever(function () {
     if (isGen == 0) {
         basic.showLeds(`
             . . . . .
@@ -85,11 +82,11 @@ basic.forever(() => {
             `)
         basic.pause(400)
     } else {
-        gen()
+        gen2()
     }
 })
-basic.forever(() => {
+basic.forever(function () {
     if (setAnim == 1) {
-        led.plot(Math.random(5), Math.random(5))
+        led.plot(Math.randomRange(0, 5), Math.randomRange(0, 5))
     }
 })
